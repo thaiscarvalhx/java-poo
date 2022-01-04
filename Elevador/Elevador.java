a2f57b6447ae40073a6674260a59a01b654263e7
Thaís Carvalho <88683497+thaiscarvalhx@users.noreply.github.com>
2022-01-04 10:36:02 -0300

    Update Elevador.java

--- Elevador/Elevador.java
+++ Elevador/Elevador.java
@@ -26,74 +26,77 @@ public class Elevador {
     }
 
     public void elevadorSobe(int s) {
-        this.andarAtual = s;
-    }
-
-    public void elevadorDesce(int d) {
-        this.andarAtual = d;
-    }
-
-    public void pessoaEntra(int e) {
-        if (pessoasPresentes < capacElevador) {
-            this.pessoasPresentes = pessoasPresentes + e;
-        } else if (pessoasPresentes > capacElevador) {
-            this.pessoasPresentes = capacElevador;
+        if (andarAtual < totalAndares) {
+            this.andarAtual = andarAtual + s;
+        } else if (andarAtual >= totalAndares) {
+            this.andarAtual = totalAndares;
         }
     }
 
-    public void pessoaSai(int a) {
-        if (pessoasPresentes < capacElevador) {
-            this.pessoasPresentes = pessoasPresentes - a;
-        } else if (pessoasPresentes > capacElevador) {
-            this.pessoasPresentes = capacElevador;
+        public void elevadorDesce(int d){
+            this.andarAtual = d;
         }
-    }
+
+        public void pessoaEntra(int e){
+            if (pessoasPresentes < capacElevador) {
+                this.pessoasPresentes = pessoasPresentes + e;
+            } else if (pessoasPresentes > capacElevador) {
+                this.pessoasPresentes = capacElevador;
+            }
+        }
+
+        public void pessoaSai(int a){
+            if (pessoasPresentes < capacElevador) {
+                this.pessoasPresentes = pessoasPresentes - a;
+            } else if (pessoasPresentes > capacElevador) {
+                this.pessoasPresentes = capacElevador;
+            }
+        }
     /*----------------------------------
                    GETTER & SETTER*/
 
-    public Integer getAndarAtual() {
-        return andarAtual;
+        public Integer getAndarAtual () {
+            return andarAtual;
+        }
+
+        public void setAndarAtual (Integer andarAtual){
+            this.andarAtual = andarAtual;
+        }
+
+        public Integer getTotalAndares () {
+            return totalAndares;
+        }
+
+        public void setTotalAndares (Integer totalAndares){
+            this.totalAndares = totalAndares;
+        }
+
+        public Integer getCapacElevador () {
+            return capacElevador;
+        }
+
+        public void setCapacElevador (Integer capacElevador){
+            this.capacElevador = capacElevador;
+        }
+
+        public Integer getPessoasPresentes () {
+            return pessoasPresentes;
+        }
+
+        public void setPessoasPresentes (Integer pessoasPresentes){
+            this.pessoasPresentes = pessoasPresentes;
+        }
+
+        //----------------------------------//
+
+        @Override
+        public String toString () {
+            System.out.println("Projetos.Elevador.Elevador{" +
+                    "Andar atual = " + andarAtual +
+                    ", total de andares = " + totalAndares +
+                    ", capacidade do elevador = " + capacElevador +
+                    ", pessoas presentes = " + pessoasPresentes +
+                    '}');
+            return null;
+        }
     }
-
-    public void setAndarAtual(Integer andarAtual) {
-        this.andarAtual = andarAtual;
-    }
-
-    public Integer getTotalAndares() {
-        return totalAndares;
-    }
-
-    public void setTotalAndares(Integer totalAndares) {
-        this.totalAndares = totalAndares;
-    }
-
-    public Integer getCapacElevador() {
-        return capacElevador;
-    }
-
-    public void setCapacElevador(Integer capacElevador) {
-        this.capacElevador = capacElevador;
-    }
-
-    public Integer getPessoasPresentes() {
-        return pessoasPresentes;
-    }
-
-    public void setPessoasPresentes(Integer pessoasPresentes) {
-        this.pessoasPresentes = pessoasPresentes;
-    }
-
-    //----------------------------------//
-
-    @Override
-    public String toString() {
-        System.out.println("Projetos.Elevador.Elevador{" +
-                "Andar atual = " + andarAtual +
-                ", total de andares = " + totalAndares +
-                ", capacidade do elevador = " + capacElevador +
-                ", pessoas presentes = " + pessoasPresentes +
-                '}');
-        return null;
-    }
-}
-
